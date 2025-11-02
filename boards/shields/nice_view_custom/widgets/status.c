aaf2e9c26ca3cdd1a19ab1c68c4d43e3be9a0afe
Ethan-Grunewald <109635031+Ethan-Grunewald@users.noreply.github.com>
2025-11-02 11:59:05 -0600

    Update status.c

--- boards/shields/nice_view_custom/widgets/status.c
+++ boards/shields/nice_view_custom/widgets/status.c
@@ -324,10 +324,11 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     sys_slist_append(&widgets, &widget->node);
     widget_battery_status_init();
     widget_output_status_init();
-    widget_layer_status_init();
-    widget_wpm_status_init();
+    //widget_layer_status_init();
+    //widget_wpm_status_init();
 
     return 0;
 }
 
 lv_obj_t *zmk_widget_status_obj(struct zmk_widget_status *widget) { return widget->obj; }
+
